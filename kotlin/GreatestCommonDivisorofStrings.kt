85d7265982b1c599cf54590226a0f4b9f2fc5d1b
Tatiana Gromova <tatiana.gromova2022@yandex.ru>
2025-09-24 21:59:00 +0300

    feat: add solution for problem 1071

---
+++ kotlin/GreatestCommonDivisorofStrings.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun gcdOfStrings(str1: String, str2: String): String {
+        var result: String = ""
+        val lengthStr1: Int = str1.length
+        val lengthStr2: Int = str2.length
+
+        if (lengthStr1 < lengthStr2) {
+            result = str2.substring(lengthStr1, lengthStr2)
+        } else {
+            result = str1.substring(lengthStr2, lengthStr1)
+        }
+
+        return result
+    }
+}
