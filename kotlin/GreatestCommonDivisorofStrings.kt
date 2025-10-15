2290f64cfd791c35b9f7f9b3e0d23e249371c77b
Ganesha1967 <115523089+Ganesha1967@users.noreply.github.com>
2025-10-15 23:31:31 +0300

    feat: solve problem 1071

--- kotlin/GreatestCommonDivisorofStrings.kt
+++ kotlin/GreatestCommonDivisorofStrings.kt
@@ -3,13 +3,19 @@
 class Solution {
     fun gcdOfStrings(str1: String, str2: String): String {
         var result: String = ""
+        var word: String = ""    
         val lengthStr1: Int = str1.length
         val lengthStr2: Int = str2.length
 
-        if (lengthStr1 < lengthStr2) {
-            result = str2.substring(lengthStr1, lengthStr2)
-        } else {
-            result = str1.substring(lengthStr2, lengthStr1)
+        for (char in str1) {
+            word += char
+            val countWord: Int = word.length
+            val count1: Int = str1.split(word).size - 1
+            val count2: Int = str2.split(word).size - 1
+
+            if (str1.contains(word) && str2.contains(word) && lengthStr1 == countWord * count1 && lengthStr2 == countWord * count2) {
+                result = word
+            }
         }
 
         return result
