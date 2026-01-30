b0940cb6d110d4c9b103f49b66c0f010daedb5da
Ganesha1967 <115523089+Ganesha1967@users.noreply.github.com>
2026-01-30 22:42:20 +0300

    refactor: optimize solution using sets for "2215. Find the Difference of Two Arrays"

--- kotlin/FindDifference.kt
+++ kotlin/FindDifference.kt
@@ -2,35 +2,12 @@
 
 class Solution {
     fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        val list1 = mutableListOf<Int>() 
-        val list2 = mutableListOf<Int>() 
+        val set1 = nums1.toSet()
+        val set2 = nums2.toSet()
 
-        for (num1 in nums1) {
-            var check_equel = 0
-            for (num2 in nums2) {
-                if (num1 == num2) {
-                    check_equel++
-                } 
-            }
+        val res_num1 = set1 - set2;
+        val res_num2 = set2 - set1;
 
-            if (check_equel == 0 && num1 !in list1) {
-                list1.add(num1)
-            }
-        }
-
-        for (num2 in nums2) {
-            var check_equel = 0
-            for (num1 in nums1) {
-                if (num1 == num2) {
-                    check_equel++
-                } 
-            }
-
-            if (check_equel == 0 && num2 !in list2) {
-                list2.add(num2)
-            }
-        }
-
-        return listOf(list1, list2)        
+        return listOf(res_num1.toList(), res_num2.toList())        
     }
 }
